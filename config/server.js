8a09f8a952a3e39ac0ec721bb3e190e1b2510021
Alejandro Yunes <alejandro.yunes@inxeption.com>
2021-10-03 01:04:06 -0500

    strapi init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c122836a2d1b631c590427d302c372eb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0b1da029-e532-49ed-86dc-bfa7d69aab0a'
+};
