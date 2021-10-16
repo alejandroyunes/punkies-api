6f2f50e0139b3b6902fb5ca0f1d2135211953b0d
Alejandro Yunes <alejandro.yunes@inxeption.com>
2021-10-16 05:18:43 -0500

    change values to english

--- config/database.js
+++ config/database.js
@@ -18,7 +18,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-// 1- uncomment above code
-// 2- Do not push the code below
-// in order to localhost to work uncomment this code.  to upload delete it and uncomment the code above
-
