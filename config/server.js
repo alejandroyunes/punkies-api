37d313eae264ba119917e40fa393c12b71afbc65
Alejandro Yunes <alejandro.yunes@inxeption.com>
2022-02-27 21:37:13 -0500

    added slug for blog

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c122836a2d1b631c590427d302c372eb'),
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    admin: {
+        auth: {
+            secret: env('ADMIN_JWT_SECRET', 'c122836a2d1b631c590427d302c372eb'),
+        },
     },
-  },
 });
